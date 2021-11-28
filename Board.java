1958358646d15901b27a6c4fe09ecc2331c8d7ce
Kevin Cao <kaiyangcao@gmail.com>
2021-11-28 18:19:15 -0500

    Board and Main Update
    Updated the board classes (tested working), created basic main class so we can all test out all of the future classes and methods.

--- Board.java
+++ Board.java
@@ -1,15 +1,32 @@
+/**
+ * The board class, which contains the private grid, constructor and
+ * the methods to update and display the basic grid.
+ */
 public class Board {
+    //the grid is private
     private char[][] grid = new char[8][8];
 
     public Board() {
-        //init constructor
+        //init constructor, temporairly all spaces, might add initial grid spaces later 
+        for(int i = 0; i < this.grid.length; i++) {
+            for (int j = 0; j < this.grid[0].length; j++) {
+                this.grid[i][j] = ' '; //empty space as we are initalizing :)
+            }
+        }
+    }
+//might add on to this, very rough way to change values
+    public void changeBoard(int posX, int posY, char value) {
+        this.grid[posX][posY] = value;
     }
 
-    public changeBoard(int posX, int posY) {
-        //code here
-    }
-
-    public void displayGrid {
-      //code here to display using for loops
+    public void update() {
+        System.out.println("------------------------");
+        for(int i = 0; i < this.grid.length; i++) {
+            for (int j = 0; j < this.grid[0].length; j++) {
+                System.out.print("[" + this.grid[i][j] + "]");
+            }
+            System.out.println(""); //newline
+        }
+        System.out.println("------------------------"); 
     }
 }
---
+++ Main.java
@@ -0,0 +1,12 @@
+/**
+ * Filler main class used to test out the methods.
+ */
+public class Main {
+    public static void main(String[] args) {
+        //init new board class :))
+        Board board = new Board();
+
+        board.update();
+
+    }
+}
