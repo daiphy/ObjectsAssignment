53a3968b9a66d8342305c4d6d862de978d417484
Kevin Cao <91091275+Windcatchr@users.noreply.github.com>
2021-11-27 22:47:06 -0500

    Create Board.java
    The board class basic methods, will fix up and add :)

---
+++ Board.java
@@ -0,0 +1,15 @@
+public class Board {
+    private char[][] grid = new char[8][8];
+
+    public Board() {
+        //init constructor
+    }
+
+    public changeBoard(int posX, int posY) {
+        //code here
+    }
+
+    public void displayGrid {
+      //code here to display using for loops
+    }
+}
