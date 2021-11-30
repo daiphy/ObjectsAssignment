aa27a009782abed8cc93f6063a95d25f7e184f84
Carethe <91379413+Carethe@users.noreply.github.com>
2021-11-30 13:21:41 -0500

    Test case update

--- Main.java
+++ Main.java
@@ -5,8 +5,12 @@ public class Main {
     public static void main(String[] args) {
         //init new board class :))
         Board board = new Board();
-
+        
+        Piece testPiece = new Piece(0, 0, 'R', "man");
+        //Test piece should show up on the board, does not show
         board.update();
+
+        System.out.println("Piece type: " + testPiece.get_pieceType());
 
     }
 }
