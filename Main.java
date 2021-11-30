e22b10ab666f0106c57cbebabea975baa7fa2a09
Carethe <91379413+Carethe@users.noreply.github.com>
2021-11-30 13:40:48 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,12 +5,11 @@ public class Main {
     public static void main(String[] args) {
         //init new board class :))
         Board board = new Board();
-        
-        Piece testPiece = new Piece(0, 0, 'R', "man");
-        //Test piece should show up on the board, does not show
-        board.update();
 
-        System.out.println("Piece type: " + testPiece.get_pieceType());
+        Piece testPiece = new Piece(0, 0, 'R', "man");
+        //Test piece should show up on the board
+        board.changeBoard(testPiece.get_xCord(), testPiece.get_yCord(), testPiece.get_pieceColor());
+        board.update();
 
     }
 }
