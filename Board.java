37ddf212e415853515a029221285881d3a2515fe
daiphy <81185432+daiphy@users.noreply.github.com>
2021-12-01 14:29:55 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -8,22 +8,46 @@ public class Board {
 
     public Board() {
         //init constructor, temporairly all spaces, might add initial grid spaces later 
-        for(int i = 0; i < this.grid.length; i++) {
-            for (int j = 0; j < this.grid[0].length; j++) {
-                this.grid[i][j] = ' '; //empty space as we are initalizing :)
+        // for(int i = 0; i < this.grid.length; i++) {
+        //     for (int j = 0; j < this.grid[0].length; j++) {
+        //         this.grid[i][j] = ' '; //empty space as we are initalizing :)
+        //     }
+        // }
+
+      // Setting the pieces in the correct spots on board
+      for (int row = 0; row < 8; row++) {
+        for (int col = 0; col < 8; col++) {
+            // conds if the remainder of the row and col are equal ie 1,1 or 1,3 
+            if ( row % 2 == col % 2 ) {
+                // its black if the row is at the top (less than 3)
+                if (row < 3) {
+                  grid[row][col] = 'b';
+                // its red if the row is at the top (more than 4)
+                } else if (row > 4) {
+                  grid[row][col] = 'r';
+                } else {
+                  grid[row][col] = ' '; // empty spaces
+                }
+            } else {
+                grid[row][col] = ' '; // empty spaces
             }
         }
+      }
     }
     
     //might add on to this, very rough way to change values
     public void changeBoard(int posX, int posY, char value) {
         this.grid[posX][posY] = value;
     }
-
-    public boolean canMoveBeMade(int posX, int posY, int newPosX, int newPosY, String colour, String type) {
+//private method that checks for if a regular move (not capture can be made)
+    private boolean canMoveBeMade(int posX, int posY, int newPosX, int newPosY, String colour, String type) {
       
+      // isnt the boad gonna be from 0-7 cuz we're counting coding mode?
+      // if (newPosX < 0 || newPosX >= 8 || newPosY < 0 || newPosY >= 8){
+      // idk - daiphy
+        
       // Checks if the move made is on the board
-      if (newPosX < 0 || newPosX > 7 || newPosY < 0 || newPosY > 7) {
+      if (newPosX < 1 || newPosX > 8 || newPosY < 1 || newPosY > 8) {
         return false;
       }
             
@@ -35,87 +59,109 @@ public class Board {
       }
       
       // Checks if the move made is forward (unless the piece is a royal)
-      if (type == "man") {
-        if (colour == "black") {
-          if (newPosY - posY != 1) {
+      //inverse for red and black, as red can only move from top to bottom and vice versa.
+      if (type.equals("man") && colour.equals("black") && newPosY - posY != 1) {
             return false;
-          }
-        }
-        else if (colour == "red") {
-          if (posY - newPosY != 1) {
+      }
+      else if (type.equals("man") && colour.equals("red") && posY - newPosY != 1) {
             return false;
-          }
-        }
       }
       
+      
       // Checks if new position isn't already taken by another piece
+
       if (grid[newPosX][newPosY] != ' ') {
         return false;
       }
-      
+      //default return true.
       return true;
     }
-    
-    /*
-    public void findForceCaptures() {
-      for (int i=0; i<grid.length; i++) {
-        for (int e=0; e<grid[i].length; e++) {
-          
-          char piece = grid[i][e];
-          
-          if (String.valueOf(piece).equals("r")) {
-            System.out.println(true);
+
+    public void makeMove(int x1, int y1, int x2, int y2, String player, String type){
+      if (canMoveBeMade(x1, y1, x2, y2, player, type)){
+        if (canCaptureBeMade(x1, y1, x2, y2, player, type)){
+          // removing the enemy piece when the player has legally jumped over it
+          if ( x1 - x2 == 2 || x1 - x2 == -2){
+            // row of the piece that needs to be eliminated
+            int eliminatedRow = (x1 + x2) / 2;
+            // column of the piece that needs to be eliminated
+            int eliminatedCol = (y1 + y2) / 2;
+            grid[eliminatedRow][eliminatedCol] = ' ';
           }
-          
-          System.out.println(grid[i][e]);
+          else {
+            System.out.println("Sorry the capture is illegal. Please input another play.");
+          }
         }
+        grid[x2][y2] = grid[x1][y1];
+        grid[x1][y1] = ' ';
+        // royal ascension
+        if( x2 == 0 && grid[x2][y2] == 'r'){
+          grid[x2][y2] = 'R';
+        }
+        if( x2 == 7 && grid[x2][y2] == 'b'){
+          grid[x2][y2] = 'B';
+        }
+      }
+      else {
+        System.out.println("Sorry the move is illegal. Please input another play.");
       }
     }
-    */
     
-    private boolean canCaptureBeMade(int posX, int posY, int newPosX, int newPosY, String colour, String type) {
+    private boolean canCaptureBeMade(int x1, int y1, int newPosX, int newPosY, String colour, String type) {
 
-      int x1 = posX+1;
-      int y1 = posY+1;
-      int x2 = posX+2;
-      int y2 = posY+2;
+      int x2 = x1+1;
+      int y2 = y1+1;
+      int x3 = x1+2;
+      int y3 = y1+2;
 
-      if (newPosX == x2 || newPosY == y2){
-      // kill can not be made if the second coords inputted are off board
-      if (x2 < 0 || x2 >= 8 || y2 < 0 || y2 >= 8){
-        return false;
-      }
-      // (x2,y2) has another piece there
-      if (grid[x2][y2] != 'r' || grid[x2][y2] != 'R' || grid[x2][y2] != 'B' ||grid[x2][y2] != 'B'){
-        return false;
-      }
-      // only applies to red pieces
-      if (colour.equals("red")){
-        // piece can not move down (can only move up)
-        if ((grid[posX][posY] == 'r' || grid[posX][posY] == 'R') && x2 > posX){
+      boolean capture = false;
+
+      if (newPosX == x3 || newPosY == y3){
+        // kill can not be made if the second coords inputted are off board
+        if (x3 < 0 || x3 >= 8 || y3 < 0 || y3 >= 8){
           return false;
         }
-        // no black pieces to jump over
-        if (grid[x1][y1] == 'b' || grid[x1][y1] == 'B'){
+        // (x2,y2) has another piece there
+        if (grid[x3][y3] == 'r' || grid[x3][y3] == 'R' || grid[x3][y3] == 'B' ||grid[x3][y3] == 'B'){
           return false;
         }
-        return true;
-      }
-      // only applies to black pieces
-      else{
-        // piece can not move up (can only move down)
-        if ((grid[posX][posY] == 'b' || grid[posX][posY] == 'B') && x2 < posX){
-          return false;
+        // only applies to red pieces
+        if (colour.equals("red")){
+          // piece can not move down (can only move up)
+          if ((grid[x1][y1] == 'r' || grid[x1][y1] == 'R') && x3 > x1){
+            return false;
+          }
+          // no black pieces to jump over
+          if (grid[x2][y2] == 'b' || grid[x2][y2] == 'B'){
+            return false;
+          }
+          capture = canCaptureBeMade(newPosX, newPosY, (newPosX+2), (newPosY+2), colour, type);
         }
-        // no red pieces to jump over
-        if (grid[x1][y1] == 'r' || grid[x1][y1] == 'R'){
-          return false;
-        }
-        return true;
+        // only applies to black pieces
+        else{
+          // piece can not move up (can only move down)
+          if ((grid[x1][y1] == 'b' || grid[x1][y1] == 'B') && x3 < x1){
+            return false;
+          }
+          // no red pieces to jump over
+          if (grid[x2][y2] == 'r' || grid[x2][y2] == 'R'){
+            return false;
+          }
+          capture = canCaptureBeMade(newPosX, newPosY, (newPosX+2), (newPosY+2), colour, type);
         }
       }
       return false;
           
+    }
+    // the public method that will be called when a move is made, if both methods return false then the move is illegal
+    public boolean checkMove(int posX, int posY, int newPosX, int newPosY, String colour, String type) {
+      if (canMoveBeMade(posX, posY, newPosX, newPosY, colour, type) == false && 
+          canCaptureBeMade(posX, posY, newPosX, newPosY, colour, type) == false) {
+        return false;
+      }
+      else {
+        return true;
+      }
     }
     
     public void update() {
