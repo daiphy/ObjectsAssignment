3210f41ec2742faf74307636ac10e05f002d93c7
Kevin Cao <kaiyangcao@gmail.com>
2021-12-01 13:10:36 -0500

    added this keyword

--- Piece.java
+++ Piece.java
@@ -22,13 +22,13 @@ public class Piece {
         //Ascension process assumes that red starts on the top, and black starts on the bottom
         if (color == 'R' && yCord == 7 && type.equals("man")){
 
-            type = "king";
+            this.type = "king";
 
         }
 
         if (color == 'B' && yCord == 0 && type.equals("man")){
 
-            type = "king";
+            this.type = "king";
 
         }
 
@@ -36,25 +36,25 @@ public class Piece {
 
     public int get_yCord(){
 
-        return yCord;
+        return this.yCord;
 
     }
 
     public int get_xCord(){
 
-        return xCord;
+        return this.xCord;
 
     }
 
     public char get_pieceColor(){
 
-        return color;
+        return this.color;
 
     }
     
     public String get_pieceType(){
 
-        return type;
+        return this.type;
 
     }
 
