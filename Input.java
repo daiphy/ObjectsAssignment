1a58d65b688738f7e7a4c2b21b85485a19b83951
epicgamerperson <93791237+epicgamerperson@users.noreply.github.com>
2021-12-01 13:13:32 -0500

    Add files via upload

--- Input.java
+++ Input.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
 
 public class Input{
-    public static char[] inputMethod(char[] a){
+    public static char[] inputMethod(char[] array){
+
+        /* 
+         * User Input Method
+         * 
+         * 
+        */
         Scanner s=new Scanner(System.in);
         boolean ask=true;
         String string="";
+        int count=0;
         do{//loop for x coord of piece
                 System.out.print("Please enter the coordinates of your piece ");
                 System.out.print("and then the coordinate you wish to move it to ");
@@ -16,21 +23,29 @@ public class Input{
                     String[] c=b[z].split(",",3);
 
                         for (int y=0;y<c.length;y++){
-                        
-                            if(c[y]!="("||c[y]!=")"){
-                                a[x]=c[y].charAt(y);
-                            }
+                            String sub=c[y];//a substituted and simplified thingie
+                            for (int x=0;x< (sub.length());x++){
+
+                                if(sub.charAt(x) !='('&&sub.charAt(x) !=')'){
+                                    
+                                    array[count]=c[y].charAt(x);
+                                }
+                                
+                        }
                         //System.out.println(c[y]);
                         }
                 }
             
 //(1,1);(2,2)              
-            ask=placeholder(a);
+            ask=placeholder(array);
         }while(ask);
         s.close();
-        return a;
+        return array;
     }
     public static boolean placeholder(char[] array){
+        /*
+         * Placeholder method for the validation inputs made by other members
+         * 
+        */
         return false;
     }
-}
