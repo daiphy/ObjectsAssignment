bf2b36c8b501fe639b7da7f064e11657e84d16d0
Kevin Cao <kaiyangcao@gmail.com>
2021-12-01 13:47:39 -0500

    added formatting

--- Main.java
+++ Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
         //init new board class :))
         Board board = new Board();
-
+        Input input = new Input();
+        
         board.update();
 
     }
