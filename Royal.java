0ddfe36e95cde6d641f5f7f37b27e1c7fc21e04c
daiphy <81185432+daiphy@users.noreply.github.com>
2021-12-02 01:37:48 -0500

    Update and rename King.java to Royal.java

--- Royal.java
+++ Royal.java
@@ -1,9 +1,9 @@
-public class King extends Piece{
+public class Royal extends Piece{
 
-    public King (int inputyCord, int inputxCord, char pieceColor, String pieceType){
+    public Royal (int inputyCord, int inputxCord, char pieceColor, String pieceType){
 
         super(inputyCord, inputxCord, pieceColor, pieceType);
-        this.type = "king";
+        this.type = "royal";
 
     }
     
