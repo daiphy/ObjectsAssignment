4de6aec751a987497fe5e0b11b78d901ab4be032
daiphy <349857383@gapps.yrdsb.ca>
2021-12-03 13:57:26 -0500

    debugging

--- Board.java
+++ Board.java
@@ -3,130 +3,165 @@
  * the methods to update and display the basic grid.
  */
 public class Board {
-    //the grid is private
-    private char[][] grid = new char[8][8];
+  //the grid is private
+  private char[][] grid = new char[8][8];
+  int redCaptures = 0;
+  int blackCaptures = 0;
+  int movesNoCaptures = 0;
+  Piece piece = new Piece();
+  private boolean isBlackTurn = true;
+  
+  public Board() {
+    //init constructor, temporairly all spaces, might add initial grid spaces later 
+    // Setting the pieces in the correct spots on board
+    for (int row = 0; row < 8; row++) {
+      for (int col = 0; col < 8; col++) {
+          // conds if the remainder of the row and col are equal ie 1,1 or 1,3 
+          if ( row % 2 == col % 2 ) {
+              // its black if the row is at the top (less than 3)
+              if (row < 3) {
+                grid[row][col] = 'b';
+              // its red if the row is at the top (more than 4)
+              } else if (row > 4) {
+                grid[row][col] = 'r';
+              } else {
+                grid[row][col] = ' '; // empty spaces
+              }
+          } else {
+              grid[row][col] = ' '; // empty spaces
+          }
+      }
+    }
+  }
+  
+  //might add on to this, very rough way to change values
+  public void changeBoard(int posX, int posY, char value) {
+      this.grid[posX][posY] = value;
 
-    public Board() {
-        //init constructor, temporairly all spaces, might add initial grid spaces later 
-        for(int i = 0; i < this.grid.length; i++) {
-            for (int j = 0; j < this.grid[0].length; j++) {
-                this.grid[i][j] = ' '; //empty space as we are initalizing :)
-            }
+  }
+
+  public boolean makeMove(int x1, int y1, int x2, int y2, String player, String type){     
+        // condition if the move can be made or capture.  
+        if (piece.canCaptureBeMade(x1, y1, x2, y2, player) || piece.canMoveBeMade(x1, y1, x2, y2, player, type)){
+          grid[x2][y2] = grid[x1][y1];
+          grid[x1][y1] = ' ';
+          // royal ascension
+          if( x2 == 0 && grid[x2][y2] == 'r'){
+            grid[x2][y2] = 'R';
+          }
+          if( x2 == 7 && grid[x2][y2] == 'b'){
+            grid[x2][y2] = 'B';
+          }
+          // removing the enemy piece when the player has legally jumped over it
+          if (piece.canCaptureBeMade(x1, y1, x2, y2, player)){
+            // row of the piece that needs to be eliminated
+            int eliminatedRow = (x1 + x2) / 2;
+            // column of the piece that needs to be eliminated
+            int eliminatedCol = (y1 + y2) / 2;
+            grid[eliminatedRow][eliminatedCol] = ' ';
+          }
+          return true;
         }
-    }
-//might add on to this, very rough way to change values
-    public void changeBoard(int posX, int posY, char value) {
-        this.grid[posX][posY] = value;
-    }
-//private method that checks for if a regular move (not capture can be made)
-    private boolean canMoveBeMade(int posX, int posY, int newPosX, int newPosY, String colour, String type) {
-      
-      // Checks if the move made is on the board
-      if (newPosX < 1 || newPosX > 8 || newPosY < 1 || newPosY > 8) {
-        return false;
-      }
-            
-      // Checks if the move made is diagonal of the current position
-      int xDifference = Math.abs(posX - newPosX);
-      int yDifference = Math.abs(posY - newPosY);
-      if (xDifference != 1 || yDifference != 1) {
+        //default case
+        System.out.println("Sorry the move/capture is illegal. Please input another play. :/");
         return false;
       }
       
-      // Checks if the move made is forward (unless the piece is a royal)
-      //inverse for red and black, as red can only move from top to bottom and vice versa.
-      if (type.equals("man") && colour.equals("black") && newPosY - posY != 1) {
-            return false;
-      }
-      else if (type.equals("man") && colour.equals("red") && posY - newPosY != 1) {
-            return false;
-      }
-      
-      
-      // Checks if new position isn't already taken by another piece
 
-      if (grid[newPosX][newPosY] != ' ') {
-        return false;
+  public void findForceCaptures() {
+    for (int i=0; i<grid.length; i++) {
+      for (int e=0; e<grid[i].length; e++) {
+        
+        char piece = grid[i][e];
+        System.out.println(piece);
+        
+        // Execeutes if the piece is red
+        if (piece == 'r' || piece == 'R') {
+
+          if (i + 2 <= 7 && e - 2 >= 0) {
+            char emptyPiece = grid[i + 2][e - 2];
+            char enemyPiece = grid[i + 1][e - 1];
+            if (emptyPiece == ' ') {
+              if (enemyPiece == 'b' || enemyPiece == 'B') {
+                System.out.println("Force take");
+              }
+            }
+          }
+          
+          if (i + 2 <= 7 && e + 2 <= 7) {
+            char emptyPiece = grid[i + 2][e + 2];
+            char enemyPiece = grid[i + 1][e + 1];
+            if (emptyPiece == ' ') {
+              if (enemyPiece == 'b' || enemyPiece == 'B') {
+                System.out.println("Force take");
+              }
+            }
+          }
+          
+          if (piece == 'R') {
+            
+            if (i - 2 >= 0 && e - 2 >= 0) {
+             
+            }
+            
+          }
+          
+
+        }
+        
+        // Execeutes if the piece is a regular black piece
+        else if (piece == 'b') {
+        
+        }
       }
-      //default return true.
+    }
+  }
+  
+  public boolean getTurn() {
+    if (this.isBlackTurn == true) {
       return true;
     }
-    
-    public boolean canCaptureBeMade(int posX, int posY, int newPosX, int newPosY, String colour) {
-      
-      // Checks if the move made is on the board
-      if (newPosX < 0 || newPosX > 7 || newPosY < 0 || newPosY > 7) {
-        System.out.println("off the board");
-        return false;
-      }
-      
-      // checks if there's an empty space at the current position
-      if (grid[posX][posY] == ' ') {
-        System.out.println("empty space");
-        return false;
-      }
-      
-      // checks if there's an empty space at the new position
-      if (grid[newPosX][newPosY] != ' ') {
-        System.out.println("empty space at new position");
-        return false;
-      }
-      
-      // conds if the difference between the 2 points is not 2
-      int xDifference = Math.abs(posX - newPosX);
-      int yDifference = Math.abs(posY - newPosY);
-      if (xDifference != 2 || yDifference != 2) {
-        System.out.println("2 part difference");
-        return false;
-      }
-      
-      // finds the middle piece coords        
-      int middleX = (posX + newPosX) / 2;
-      int middleY = (posY + newPosY) / 2;
-
-      char piece = grid[posX][posY];
-      if (colour.equals("red")) {
-        
-        char middlePiece = grid[middleX][middleY];
-        if (middleX > posX && piece == 'r') {
-          return false;
-        }
-        
-        if (middlePiece == 'b' || middlePiece == 'B') {
-          return true;
-        }
-      }
-      else {
-        
-        char middlePiece = grid[middleX][middleY];
-        if (middleX < posX && piece == 'b') {
-          return false;
-        }
-        
-        if (middlePiece == 'r' || middlePiece == 'R') {
-          return true;
-        }     
-      }
+    else {
       return false;
     }
-    // the public method that will be called when a move is made, if both methods return false then the move is illegal
-//     public boolean checkMove(int posX, int posY, int newPosX, int newPosY, String colour, String type) {
-//       if (canMoveBeMade(posX, posY, newPosX, newPosY, colour, type) == false && 
-//           canCaptureBeMade(posX, posY, newPosX, newPosY, colour) == false) {
-//         return false;
-//       }
-//       else {
-//         return true;
-//       }
-//     }
-      public void update() {
-        System.out.println("------------------------");
-        for(int i = 0; i < this.grid.length; i++) {
-            for (int j = 0; j < this.grid[0].length; j++) {
-                System.out.print("[" + this.grid[i][j] + "]");
-            }
-            System.out.println(""); //newline
-        }
-        System.out.println("------------------------"); 
+  }
+  
+  public char getSquareId (int posX, int posY) {
+    return this.grid[posX][posY];
+  }
+  
+  public void changeTurn() {
+    this.isBlackTurn = !(this.isBlackTurn);
+  }
+  
+  public int update() {
+      System.out.println("------------------------  " + redCaptures); //Shows the number of red pieces captured
+      for(int i = 0; i < this.grid.length; i++) {
+          for (int j = 0; j < this.grid[0].length; j++) {
+              System.out.print("[" + this.grid[i][j] + "]");
+          }
+          System.out.println(""); //newline
+      }
+      System.out.println("------------------------  " + blackCaptures);
+      int gameStatus = endCondition(redCaptures, blackCaptures, movesNoCaptures); 
+      return gameStatus;
+  }
+
+  public int endCondition(int redCaptures, int blackCaptures, int movesNoCaptures) {
+    /**This method checks whether any of the conditions below have been met to end the game
+     * - All pieces of one color have been captured
+     * - 10 moves were made without capturing any pieces
+     * It will return a 1 for a black victory, 2 for a red victory, 3 for a tie, and 0 if the game continues
+     */
+    if (redCaptures == 12){
+      return 1;
     }
+    if (blackCaptures == 12){
+      return 2;
+    }
+    if (movesNoCaptures == 10){
+      return 3;
+    }
+    return 0;
+  }
 }
