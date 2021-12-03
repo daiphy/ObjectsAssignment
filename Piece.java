d5cc0dd94cc3eb9fe7d97bd8f27041d390d6d336
daiphy <349857383@gapps.yrdsb.ca>
2021-12-03 13:57:13 -0500

    debugging

--- Piece.java
+++ Piece.java
@@ -8,19 +8,6 @@ public class Piece {
     protected String type;
 
     public Piece (){
-  // int inputyCord, int inputxCord, char pieceColor, String pieceType
-        /**
-         * This assumes that the object initializer includes the coordinates of the piece
-         * As well as the color and type of the piece
-         * Assumed format for piece traits is (row number, column number, piece color, piece type)
-         */
-
-//        yCord = inputyCord;
-//        xCord = inputxCord;
-//        color = pieceColor;
-//        type = pieceType;
-
-
         //Ascension process assumes that red starts on the top, and black starts on the bottom
     }
 
@@ -33,10 +20,6 @@ public class Piece {
     public int get_xCord(){
 
         return this.xCord;
-        // yCord = inputyCord;
-        // xCord = inputxCord;
-        // color = pieceColor;
-        // type = pieceType;
 
     }
 
@@ -46,25 +29,9 @@ public class Piece {
      * - James Z
      */
 
-    // public int get_yCord(){
-    //     return this.yCord;
-    // }
-
-    // public int get_xCord(){
-    //     return this.xCord;
-    // }
-
-    // public char get_pieceColor(){
-    //     return this.color;
-  // }
-
-    // }
-    // public String get_pieceType(){
-    //     return this.type;
-
-    // }
-    //private method that checks for if a regular move (not capture can be made)
-    public boolean canMoveBeMade(int posX, int posY, int newPosX, int newPosY, String colour, String type) {
+   
+    //protected method that checks for if a regular move (not capture can be made)
+    protected boolean canMoveBeMade(int posX, int posY, int newPosX, int newPosY, String colour, String type) {
       
         // Checks if the move made is on the board
         if (newPosX < 1 || newPosX > 8 || newPosY < 1 || newPosY > 8) {
@@ -97,99 +64,63 @@ public class Piece {
         return true;
       }
   
-    //   public void makeMove(int x1, int y1, int x2, int y2, String player, String type){
-    //     if (canMoveBeMade(x1, y1, x2, y2, player, type)){
-    //       // removing the enemy piece when the player has legally jumped over it
-    //       if ( x1 - x2 == 2 || x1 - x2 == -2){
-    //         if (canCaptureBeMade(x1,y1,x1+1,y1+1,x1+2,y1+2, player, type) == true 
-    //         || canCaptureBeMade(x1,y1,x1-1,y1+1,x1-2,y1+2, player, type) == true
-    //         || canCaptureBeMade(x1,y1,x1+1,y1-1,x1+2,y1-2, player,type) == true
-    //         || canCaptureBeMade(x1,y1,x1-1,y1-1,x1-2,y1-2, player, type) == true ){
-    //           // row of the piece that needs to be eliminated
-    //           int eliminatedRow = (x1 + x2) / 2;
-    //           // column of the piece that needs to be eliminated
-    //           int eliminatedCol = (y1 + y2) / 2;
-    //           grid[eliminatedRow][eliminatedCol] = ' ';
-    //         }
-    //         else {
-    //           System.out.println("Sorry the capture is illegal. Please input another play.");
-    //         }
-    //       }
-    //       grid[x2][y2] = grid[x1][y1];
-    //       grid[x1][y1] = ' ';
-    //       // royal ascension
-    //       if( x2 == 0 && grid[x2][y2] == 'r'){
-    //         grid[x2][y2] = 'R';
-    //       }
-    //       if( x2 == 7 && grid[x2][y2] == 'b'){
-    //         grid[x2][y2] = 'B';
-    //       }
-    //     }
-    //     else {
-    //       System.out.println("Sorry the move is illegal. Please input another play.");
-    //     }
-    //   }
+    protected boolean canCaptureBeMade(int posX, int posY, int newPosX, int newPosY, String colour) {
       
-      public boolean canCaptureBeMade(int x1, int y1, int x2, int y2, int x3, int y3, String colour, String type) {
-  
-          // kill can not be made if the second coords inputted are off board
-          if (x3 < 0 || x3 >= 8 || y3 < 0 || y3 >= 8){
-            return false;
-          }
-          // (x2,y2) has another piece there
-          if (grid[x3][y3] == 'r' || grid[x3][y3] == 'R' || grid[x3][y3] == 'B' ||grid[x3][y3] == 'B'){
-            return false;
-          }
-          if (type.equalsIgnoreCase("pawn")){ 
-            // only applies to red pieces
-            if (colour.equalsIgnoreCase("red")){
-              // piece can not move down (can only move up)
-              if ((grid[x1][y1] == 'r') && x3 > x1){
-                return false;
-              }
-              // no black pieces to jump over
-              if (grid[x2][y2] == 'b' || grid[x2][y2] == 'B'){
-                return false;
-              }
-              return true;
-              // capture = canCaptureBeMade(x3, y3, (x3+2), (y3+2), colour, type);
-            }
-            // only applies to black pieces
-            else{
-              // piece can not move up (can only move down)
-              if ((grid[x1][y1] == 'b') && x3 < x1){
-                return false;
-              }
-              // no red pieces to jump over
-              if (grid[x2][y2] == 'r' || grid[x2][y2] == 'R'){
-                return false;
-              }
-              return true;
-              // capture = canCaptureBeMade(newPosX, newPosY, (newPosX+2), (newPosY+2), colour, type);
-            }
-          }
-          else if(type.equalsIgnoreCase("royal")) {
-            // only applies to red pieces
-            if (colour.equalsIgnoreCase("red")){
-              if (grid[x2][y2] == 'b' || grid[x2][y2] == 'B'){
-                return false;
-              }
-              return true;
-              // capture = canCaptureBeMade(x3, y3, (x3+2), (y3+2), colour, type);
-            }
-            // only applies to black pieces
-            else{
-              // no red pieces to jump over
-              if (grid[x2][y2] == 'r' || grid[x2][y2] == 'R'){
-                return false;
-              }
-              return true;
-              // capture = canCaptureBeMade(newPosX, newPosY, (newPosX+2), (newPosY+2), colour, type);
-            }
-          }
-          return false;
-            
+      // Checks if the move made is on the board
+      if (newPosX < 0 || newPosX > 7 || newPosY < 0 || newPosY > 7) {
+        System.out.println("off the board");
+        return false;
       }
+      
+      // checks if there's an empty space at the current position
+      if (grid[posX][posY] == ' ') {
+        System.out.println("empty space");
+        return false;
+      }
+      
+      // checks if there's an empty space at the new position
+      if (grid[newPosX][newPosY] != ' ') {
+        System.out.println("empty space at new position");
+        return false;
+      }
+      
+      // conds if the difference between the 2 points is not 2
+      int xDifference = Math.abs(posX - newPosX);
+      int yDifference = Math.abs(posY - newPosY);
+      if (xDifference != 2 || yDifference != 2) {
+        System.out.println("2 part difference");
+        return false;
+      }
+      
+      // finds the middle piece coords        
+      int middleX = (posX + newPosX) / 2;
+      int middleY = (posY + newPosY) / 2;
+
+      char piece = grid[posX][posY];
+      if (colour.equals("red")) {
+        
+        char middlePiece = grid[middleX][middleY];
+        if (middleX > posX && piece == 'r') {
+          return false;
+        }
+        
+        if (middlePiece == 'b' || middlePiece == 'B') {
+          return true;
+        }
+      }
+      else {
+        
+        char middlePiece = grid[middleX][middleY];
+        if (middleX < posX && piece == 'b') {
+          return false;
+        }
+        
+        if (middlePiece == 'r' || middlePiece == 'R') {
+          return true;
+        }     
+      }
+      return false;
+    }
 
 }
 
